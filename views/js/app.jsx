363a1ad19dd2de1a86c18362738291facd1ac6b6
Tom King <design.tomking@gmail.com>
2019-01-05 18:15:53 -0700

    App parses hash and sets localstorage auth tokens
    renders either home or loggedin components

--- views/js/app.jsx
+++ views/js/app.jsx
@@ -1,4 +1,65 @@
+const AUTH0_CLIENT_ID = "c9KOWojP2hFVgdk5J3kPPPvN1697cZJZ";
+const AUTH0_DOMAIN = "dad-jokes.auth0.com";
+const AUTH0_CALLBACK_URL = location.href;
+const AUTH0_API_AUDIENCE = "https://dad-jokes/api";
+
 class App extends React.Component {
+  parseHash() {
+    this.auth0 = new auth0.WebAuth({
+      domain: AUTH0_DOMAIN,
+      clientID: AUTH0_CLIENT_ID
+    });
+    this.auth0.parseHash(window.location.hash, (err, authResult) => {
+      if (err) {
+        return console.log(err);
+      }
+      if (
+        authResult !== null &&
+        authResult.accessToken !== null &&
+        authResult.idToken !== null
+      ) {
+        localStorage.setItem("access_token", authResult.accessToken);
+        localStorage.setItem("id_token", authResult.idToken);
+        localStorage.setItem(
+          "profile",
+          JSON.stringify(authResult.idTokenPayload)
+        );
+        window.location = window.location.href.substr(
+          0,
+          window.location.href.indexOf("#")
+        );
+      }
+    });
+  }
+
+  setup() {
+    $.ajaxSetup({
+      beforeSend: (r) => {
+        if (localStorage.getItem("access_token")) {
+          r.setRequestHeader(
+            "Authorization",
+            "Bearer " + localStorage.getItem("access_token")
+          );
+        }
+      }
+    });
+  }
+
+  setState() {
+    let idToken = localStorage.getItem("id_token");
+    if (idToken) {
+      this.loggedIn = true;
+    } else {
+      this.loggedIn = false;
+    }
+  }
+
+  componentWillMount() {
+    this.setup();
+    this.parseHash();
+    this.setState();
+  }
+
   render() {
     if (this.loggedIn) {
       return (<LoggedIn />);
